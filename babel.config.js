63956cf02a838bb4bdb6e883c62f84bdb6388788
marrcelosantana <marcelosantana@alu.ufc.br>
2023-05-10 13:56:18 -0300

    [Save Pass App] - first commit

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,29 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "babel-plugin-styled-components",
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@models": "./src/models",
+            "@assets": "./src/assets",
+            "@components": "./src/components",
+            "@screens": "./src/screens",
+            "@storage": "./src/storage",
+            "@utils": "./src/utils",
+            "@services": "./src/services",
+            "@hooks": "./src/hooks",
+            "@contexts": "./src/contexts",
+            "@routes": "./src/routes",
+            "@themes": "./src/themes",
+            "@app": "./src/app",
+          },
+        },
+      ],
+    ],
   };
 };
---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,9 @@
+import { Container, Title } from "./styles";
+
+export function Register() {
+  return (
+    <Container>
+      <Title>Register</Title>
+    </Container>
+  );
+}

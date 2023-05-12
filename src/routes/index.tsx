8fb7ca983768ad9adf81a4ccd6807e95ee9a51af
marrcelosantana <marcelosantana@alu.ufc.br>
2023-05-12 15:56:26 -0300

    [Save Pass App] - Bug fix on routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,21 @@
+import { View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 
 import { useAuth } from "@hooks/useAuth";
+import { useTheme } from "styled-components/native";
 
 export function Routes() {
   const { user } = useAuth();
+  const theme = useTheme();
 
   return (
-    <NavigationContainer>
-      {user ? <AppRoutes /> : <AuthRoutes />}
-    </NavigationContainer>
+    <View style={{ flex: 1, backgroundColor: theme.COLORS.BACKGROUND }}>
+      <NavigationContainer>
+        {user.id ? <AppRoutes /> : <AuthRoutes />}
+      </NavigationContainer>
+    </View>
   );
 }

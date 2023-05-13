65383cfbd227245d36bbca7e17df1eb8161c459b
marrcelosantana <marcelosantana@alu.ufc.br>
2023-05-13 16:11:38 -0300

    [Save Pass App] - Add react-hook-form and yup

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -4,6 +4,10 @@ import { Pressable, View } from "react-native";
 import { useTheme } from "styled-components/native";
 import { CaretLeft, Eye, EyeSlash } from "phosphor-react-native";
 
+import * as yup from "yup";
+import { Controller, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 import { useNavigation } from "@react-navigation/native";
 import { AppNavigatorRoutesProps } from "@routes/app.routes";
 
@@ -20,14 +24,43 @@ import {
   Label,
 } from "./styles";
 
+interface FormDataProps {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const registerSchema = yup.object({
+  name: yup.string().trim().required("Informe o nome"),
+  email: yup
+    .string()
+    .trim()
+    .email("Email inválido")
+    .required("Informe o email"),
+  password: yup
+    .string()
+    .trim()
+    .required("Informe a senha")
+    .min(6, "A senha deve ter pelo menos 6 dígitos"),
+});
+
 export function Register() {
   const [passwordIsHidden, setPasswordIsHidden] = useState(true);
+
+  const { control, handleSubmit, reset } = useForm<FormDataProps>({
+    resolver: yupResolver(registerSchema),
+  });
 
   const theme = useTheme();
   const navigator = useNavigation<AppNavigatorRoutesProps>();
 
   function handleShowPassword() {
     setPasswordIsHidden(!passwordIsHidden);
+  }
+
+  function handleRegister(data: FormDataProps) {
+    console.log(data);
+    reset();
   }
 
   return (
@@ -43,39 +76,70 @@ export function Register() {
       <Content>
         <Form>
           <Label>Nome do serviço</Label>
-          <Input />
+          <Controller
+            control={control}
+            name="name"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Digite o nome..."
+                value={value}
+                onChangeText={onChange}
+              />
+            )}
+          />
         </Form>
 
         <Form>
           <Label>E-mail</Label>
-          <Input />
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Digite o e-mail..."
+                value={value}
+                onChangeText={onChange}
+                keyboardType="email-address"
+                autoCapitalize="none"
+              />
+            )}
+          />
         </Form>
 
         <Form>
           <Label>Senha</Label>
-          <Input
-            secureTextEntry={passwordIsHidden}
-            rightElement={
-              <Pressable onPress={handleShowPassword}>
-                {passwordIsHidden ? (
-                  <EyeSlash
-                    size={22}
-                    color={theme.COLORS.TEXT_BODY}
-                    style={{ marginRight: 10 }}
-                  />
-                ) : (
-                  <Eye
-                    size={24}
-                    color={theme.COLORS.TEXT_BODY}
-                    style={{ marginRight: 10 }}
-                  />
-                )}
-              </Pressable>
-            }
+          <Controller
+            control={control}
+            name="password"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Digite a senha..."
+                value={value}
+                onChangeText={onChange}
+                secureTextEntry={passwordIsHidden}
+                rightElement={
+                  <Pressable onPress={handleShowPassword}>
+                    {passwordIsHidden ? (
+                      <EyeSlash
+                        size={22}
+                        color={theme.COLORS.TEXT_BODY}
+                        style={{ marginRight: 10 }}
+                      />
+                    ) : (
+                      <Eye
+                        size={24}
+                        color={theme.COLORS.TEXT_BODY}
+                        style={{ marginRight: 10 }}
+                      />
+                    )}
+                  </Pressable>
+                }
+              />
+            )}
           />
         </Form>
 
-        <Button>
+        <Button onPress={handleSubmit(handleRegister)}>
           <ButtonText>Salvar</ButtonText>
         </Button>
       </Content>
